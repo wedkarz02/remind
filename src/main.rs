cca72732db89c764522da7a8c28a81921f1d6aeb
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-04-27 22:24:08 +0200

    feat: tar reminder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// TODO: Make this actually usable later
 fn main() {
-    println!("dupa");
+    let mut args = std::env::args();
+    match args.nth(1).expect("doodoo brain, no arg detected").as_str() {
+        "tar" => {
+            println!("pack   -> tar -czvf archive.tar.gz /path/to/directory_or_file");
+            println!("unpack -> tar -xzvf archive_name.tar.gz");
+        }
+        _ => println!("don't know this one bruh"),
+    }
 }
